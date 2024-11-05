e844cf29f34c020e7a2425ddd3499198e23d0554
softgenicsShubham <softgenics@gmail.com>
2024-11-05 17:59:03 +0530

    new server file update

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 // server.js
-const SERVER_API_URL = 'https://dc.dceyewr.com'; 
-// const SERVER_API_URL = 'http://localhost:8000'; 
+// const SERVER_API_URL = 'https://dc.dceyewr.com'; 
+const SERVER_API_URL = 'http://localhost:8000'; 
 // const SERVER_API_URL = 'backend.dceyewr.com'; 
 
 export { SERVER_API_URL };
